a0c39fb7319d8ed0718ed63f9a3771af7b33e286
kimleng.rin@gmail.com <kimleng.rin@gmail.com>
2018-01-18 15:17:04 +0700

    Add leave types records to database

---
+++ Migrations/20180118080202_Populate Leave Types.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Khres.Migrations
+{
+    public partial class PopulateLeaveTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {            
+            migrationBuilder.Sql("INSERT INTO LeaveTypes(Label) VALUES('Annual Leave')");
+            migrationBuilder.Sql("INSERT INTO LeaveTypes(Label) VALUES('Sick Leave')");
+            migrationBuilder.Sql("INSERT INTO LeaveTypes(Label) VALUES('Special Leave')");
+            migrationBuilder.Sql("INSERT INTO LeaveTypes(Label) VALUES('Public Holiday')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE LeaveTypes WHERE Id IN (SELECT Id FROM LeaveTypes WHERE Label IN ('Annual Leave', 'Sick Leave', 'Special Leave', 'Public Holiday'))");
+        }
+    }
+}
