dbb57f5873535b023739ca39ea20cf17e30c628e
kimleng.rin@gmail.com <kimleng.rin@gmail.com>
2018-01-19 15:40:45 +0700

    Implement update and delete leave controller

--- Controllers/LeaveController.cs
+++ Controllers/LeaveController.cs
@@ -5,6 +5,7 @@ using Khres.Controllers.Resources;
 using Khres.Models;
 using Khres.Persistent;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Khres.Controllers
 {
@@ -29,6 +30,35 @@ namespace Khres.Controllers
             leave.LastUpdated = DateTime.Now; 
             context.Leaves.Add(leave);
                
+            await context.SaveChangesAsync();
+            var result = mapper.Map<Leave, CreateLeaveDto>(leave);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateLeave(int id, [FromBody] CreateLeaveDto createLeaveDto) {
+            if(!ModelState.IsValid) {
+                return BadRequest(ModelState);
+            }
+
+            var leave = await context.Leaves.Include(x => x.EmployeeLeaves).SingleOrDefaultAsync(x => x.Id == id);
+            mapper.Map<CreateLeaveDto, Leave>(createLeaveDto, leave);
+
+            await context.SaveChangesAsync();
+            var result  = mapper.Map<Leave, CreateLeaveDto>(leave);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLeave(int id) {
+            
+            if(!ModelState.IsValid) {
+                return BadRequest(ModelState);
+            }
+
+            var leave = await context.Leaves.FindAsync(id);
+            context.Leaves.Remove(leave);
+            
             await context.SaveChangesAsync();
             var result = mapper.Map<Leave, CreateLeaveDto>(leave);
             return Ok(result);
--- Mapping/KhresMappingProfile.cs
+++ Mapping/KhresMappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
 using Khres.Controllers.Resources;
@@ -15,9 +16,29 @@ namespace Khres.Mapping
             
             CreateMap<LeaveDetailDto, LeaveDetail>().ReverseMap();
             CreateMap<CreateLeaveDto, Leave>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .AfterMap((src, dest) => {
+                var removedLeaveTypes = new List<EmployeeLeave>();
+                foreach(var s in src.LeaveTypeIds) {
+                    var itemExisted = dest.EmployeeLeaves.Any(x => x.LeaveTypeId == s);
+                    if(!itemExisted) dest.EmployeeLeaves.Add(new EmployeeLeave() {LeaveTypeId = s});
+                }
+
+                foreach(var d in dest.EmployeeLeaves) {
+                    var itemExisted = src.LeaveTypeIds.Contains(d.LeaveTypeId);
+                    if(!itemExisted) {
+                        removedLeaveTypes.Add(d);
+                    }    
+                }
+
+                removedLeaveTypes.ForEach(el => {
+                    dest.EmployeeLeaves.Remove(el);
+                });                
+            });
+            /*
             .ForMember(dest => dest.EmployeeLeaves, opt => opt.MapFrom(s => s.LeaveTypeIds.Select(x => new EmployeeLeave() {
                 LeaveTypeId = x
-            })));
+            }))); */
 
             CreateMap<Leave, CreateLeaveDto>()
             .ForMember(dest => dest.LeaveTypeIds, opt => opt.MapFrom(s => s.EmployeeLeaves.Select(x => x.LeaveTypeId)));
